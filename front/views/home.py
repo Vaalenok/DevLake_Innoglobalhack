4aa039396df239cb3506621c865ded694d58ffdd
AleksRiffel <avr-tum@mail.ru>
2024-11-03 04:19:07 +0500

    Update home.py

--- front/views/home.py
+++ front/views/home.py
@@ -1,51 +1,38 @@
+import asyncio
+
 import flet as ft
+from  src.db_interface import get_all_users
 
 def home_view(page):
-    global rows  # Объявляем переменную rows как глобальную
-
-    employee_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    employee_descriptions = [
-        "Опытный программист",
-        "Младший аналитик",
-        "Менеджер по продажам",
-        "Бухгалтер",
-        "Системный администратор",
-        "Дизайнер интерфейсов",
-        "Руководитель отдела",
-        "Стажер-юрист",
-        "Маркетолог",
-        "Специалист по обработке данных"
-    ]
+    def update_table(e=None):
+        users = asyncio.run(get_all_users())
+        filtered_users = [user for user in users if search_text.value.lower() in user.full_name.lower()]
+        table.rows = [
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(user.full_name)),
+                    ft.DataCell(ft.Text(user.company)),
+                    ft.DataCell(ft.Text(user.experience)),
+                ],
+                on_select_changed=lambda e, user_id=user.id: page.go(f"/employee?param={user_id}"),
+            ) for user in filtered_users
+        ]
+        page.update()
 
     search_text = ft.TextField(hint_text="Поиск по ФИО", on_change=lambda e: update_table())
 
     columns = [
         ft.DataColumn(ft.Text("ФИО")),
-        ft.DataColumn(ft.Text("Описание")),
-    ]
-
-    rows = [
-        ft.DataRow(
-            cells=[
-                ft.DataCell(ft.Text(f"Сотрудник {emp_id}")),
-                ft.DataCell(ft.Text(employee_descriptions[emp_id - 1])),
-            ],
-            on_select_changed=lambda e, id=emp_id: page.go(f"/employee?param={id}"),
-        ) for emp_id in employee_ids
+        ft.DataColumn(ft.Text("Компания")),
+        ft.DataColumn(ft.Text("Стаж")),
     ]
 
     table = ft.DataTable(
         sort_column_index=0,
         sort_ascending=True,
         columns=columns,
-        rows=rows
+        rows=[],
     )
-
-    def update_table(e=None):
-        global rows
-        filtered_rows = [row for row in rows if search_text.value.lower() in row.cells[0].content.value.lower()]
-        table.rows = filtered_rows
-        page.update()
 
     return (
         ft.View(
