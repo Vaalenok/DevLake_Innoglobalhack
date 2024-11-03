d15c036147c5a520033e238cb08904e0d1403a8e
Pankyxa <kbelozerov1@gmail.com>
2024-11-03 04:47:16 +0300

    fix

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from sqlalchemy import text
 from database import engine, Base, AsyncSessionLocal
 from initial_create_db import init_create_db
 import src.db_interface as db
+from src.models import user, feedback, criteria_type, score, feedback_score, score_history
 
 
 async def init_db():
-    async with AsyncSessionLocal() as db:
+    async with AsyncSessionLocal() as session:
         async with engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
 
-        result = await db.execute(text("select count(*) from users"))
+        result = await session.execute(text("select count(*) from users"))
         user_count = result.scalar()
 
         if user_count == 0:
-            await init_create_db(db)
-
+            await init_create_db(session)
 
 
 async def main(page: ft.Page):
@@ -28,5 +28,6 @@ async def main(page: ft.Page):
     page.on_view_pop = rtg.view_pop
     page.go(page.route)
 
+
 if __name__ == "__main__":
-    asyncio.run(ft.app(target=main, view=ft.AppView.WEB_BROWSER))
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER)
